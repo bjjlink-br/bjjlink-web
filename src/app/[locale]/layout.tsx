dc8bd3fd1c305e8df369f48f12fb7590fd93519a
Bernardo <bernardorizzatti01@gmail.com>
2026-04-10 17:08:56 -0300

    feat: google analytics (#5)

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -7,6 +7,7 @@ import { routing } from "@/i18n/routing";
 import { notFound } from "next/navigation";
 import { DataSectionsProvider } from "@/contexts/DataSectionsContext";
 import Providers from "@/utils/providers";
+import Script from "next/script";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -40,7 +41,22 @@ export default async function RootLayout({
 
   return (
       <html lang={locale}>
+        <Script id="gtm" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P5FCM95F');`}
+        </Script>
         <body className={`antialiased bg-gray-1300 text-gray-50 ${poppins.variable} ${openSans.variable}`} >
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-P5FCM95F"
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            />
+          </noscript>
           <DataSectionsProvider>
             <NextIntlClientProvider messages={messages}>
               <Providers>
